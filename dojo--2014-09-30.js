b3c5eaa053b5df53cbc47947ee7af9c262d5b0b1
Carlos Donze <xdonze@live.com>
2014-09-30 17:34:31 -0400

    Initial commit @kreider1997 @xdonze

--- dojo--2014-09-30.js
+++ dojo--2014-09-30.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+/**
+ * String Calculator
+ *
+ * Given two strings that represent numbers in English -- "one", "two", "three" --
+ * calculate the addition, subtraction, multiplication, and division of those numbers
+ * and return that value as a `Number`. For example:
+ *
+ * add("one", "one") === 2
+ *
+ * For complete credit, provide test coverage for all numbers "zero" through "ten"
+ * for at least _two_ operations, but keep in mind that the assignment is designed
+ * to enforce _practice_ not _completeness_. Follow "red-green-refactor", use
+ * ping-pong pairing, and commit often using `@username & @username` as the
+ * commit message.
+ */
+
+//console.log('"For my ally is the Force; and a powerful ally, it is."')
+
+
+
+
+console.log('it should divide "one" and "one": ',
+    divide("one, one") === 1);
