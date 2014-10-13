dca99b5a508e40b67a3c7cecd5bcc94bab79e67a
Carlos Donze <xdonze@live.com>
2014-10-13 13:51:37 -0400

    Add project_euler.js file and edit comment text.

---
+++ project_euler.js
@@ -0,0 +1,22 @@
+/*
+This is where the EULER assignment will go
+
+
+=== PRODUCTION CODE ===
+function solution(){
+    // change input to suit the solution, e.g. `limit`
+    return {
+        // I wanna see this one...
+        solveFor: function(limit){ ... },
+
+        // For Problem 2, for example... However you solve it.
+        fibonacci: function(limit){ . . .  },
+        filterEvens: function(list){ . . . },
+        sum: function(list){ . . . }
+    }
+}
+
+
+=== TEST CODE ===
+You should probably write some..
+*/
